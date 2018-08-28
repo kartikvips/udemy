085c8814c6220e92417eea28d9a7a3cd0edb01b0
kartikvips <kartikvips@gmail.com>
2018-08-27 17:41:12 -0700

    add test

---
+++ modernreact/test.rb
@@ -0,0 +1,16 @@
+def unique_items(ary)
+    dict = Hash.new(0)
+    for x in ary
+        dict[x] += 1
+    end
+    return_arr = []
+    for x in ary
+        if dict[x] == 1
+            return_arr << x
+        end
+    end
+    return return_arr
+end
+
+
+p unique_items([1,2,3,4,5,5,2,3])
